d3fd6ca9026d9bbb7807d9aad103da951a90a20d
Yuka Takiyama <yukatakiyama27@gmail.com>
2020-02-19 17:28:47 +0900

    seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+flat_attributes = [
+  { name: 'Light & Spacious Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 75,
+    number_of_guests: 3
+  },
+  { name: 'St Pancras Clock Tower Guest Suite',
+    address: 'Euston Rd London N1C 4QP',
+    description: 'A mini apartment within the clock tower apartment at St Pancras Station, with its own en suite bathroom and kitchen and sitting area.',
+    price_per_night: 110,
+    number_of_guests: 2
+  },
+  { name: 'Cozy room',
+    address: '42 Dreieich Strasse London W9 1DT',
+    description: 'lovely nice apartment',
+    price_per_night: 80,
+    number_of_guests: 3
+  },
+  { name: 'Ghetto feeling apartment',
+    address: '10 Dawson Geelong Australia W9 1DT',
+    description: 'have a nice garden for BBQ',
+    price_per_night: 50,
+    number_of_guests: 4
+  },
+  { name: 'Nice Ocean view room',
+    address: 'Heaven beach',
+    description: 'beautiful blue and white room, free beer',
+    price_per_night: 90,
+    number_of_guests: 3
+  }
+]
+
+Flat.create!(flat_attributes)
